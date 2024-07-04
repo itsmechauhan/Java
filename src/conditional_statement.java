021efa1f03a6922d1b3bbafc42ea29cf6235b32d
itsmechauhan <techhchacker@gmail.com>
2024-07-05 00:23:24 +0530

    updation to code

--- src/conditional_statement.java
+++ src/conditional_statement.java
@@ -1,5 +1,8 @@
+import java.awt.desktop.ScreenSleepEvent;
+import java.util.Scanner;
+
 public class conditional_statement {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         /*
         //wap to check candidate is eligible for vote or not
         System.out.println("Enter the Age: ");
@@ -17,5 +20,21 @@ public class conditional_statement {
         }
 
          */
+        /*
+        //wap to check whether the no is even or odd
+        System.out.println("Enter num: ");
+        Scanner ac = new Scanner(System.in);
+        int n = ac.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("the num (" + n + ") is EVEN  ");
+        }
+        else{
+            System.out.println("the num (" + n + ") is ODD 12");
+        }
+
+
+         */
+
+
     }
 }
