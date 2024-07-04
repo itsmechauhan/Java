6a6fdb5ffa518aaf56b528826b6df501ae8e2195
itsmechauhan <techhchacker@gmail.com>
2024-07-05 00:17:14 +0530

    intro to conditional_statement.java

---
+++ src/conditional_statement.java
@@ -0,0 +1,21 @@
+public class conditional_statement {
+    public static void main(String[] args){
+        /*
+        //wap to check candidate is eligible for vote or not
+        System.out.println("Enter the Age: ");
+        Scanner ac=new Scanner(System.in);
+        int age=ac.nextInt();
+        if(age<18&&age>0){
+            System.out.println("Not Eligible!!😭😭");
+        }
+        else if(age<0){
+            System.out.println("Invalid Age!!😡");
+        }
+        else{
+
+            System.out.println("Eligible for voting!!😁🎉");
+        }
+
+         */
+    }
+}
