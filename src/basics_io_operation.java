4ed6ec18430294784857544e73d1baf971395864
itsmechauhan <techhchacker@gmail.com>
2024-07-04 23:56:04 +0530

    revision of java basics_io_operation

--- src/basics_io_operation.java
+++ src/basics_io_operation.java
@@ -1,6 +1,82 @@
+
+import java.util.Scanner;
+
 public class basics_io_operation {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        /*
+        //Swapping two number
+        int a=10;
+        int b=20;
+        System.out.println("No before swapping!!\nA="+a+"\nB= "+b);
+        a=a^b;
+        b=a^b;
+        a=a^b;
+        System.out.println("swapped no are : \nA= "+a+"\nB= "+b);
+*/
 
+        /*
+        //input two num and print sum
+        int a,b;
+        Scanner ac= new Scanner(System.in);
+        System.out.println("Enter A: ");
+        a=ac.nextInt();
+        System.out.println("Enter B: ");
+        b=ac.nextInt();
+        System.out.println("Sum= "+(a+b));
+
+
+         */
+/*
+        //wap to input l,b then calc area of rectangle
+        System.out.println("Enter Length: ");
+        Scanner input=new Scanner(System.in);
+        int l=input.nextInt();
+        System.out.println("Enter Breadth: ");
+        int b=input.nextInt();
+        System.out.println("Area of rectangle: "+(l*b));
+
+
+
+ */
+/*
+
+
+        //wap to input side aand then calc perimeter and its area
+        System.out.println("Enter Side: ");
+        Scanner ac=new Scanner(System.in);
+        int side=ac.nextInt();
+        System.out.println("Periment of a square: "+ 4*side);
+        System.out.println("Area of a Square: "+side*side);
+
+
+ */
+/*
+
+        //wap to find SI
+        Scanner ac=new Scanner(System.in);
+        System.out.println("Enter Principle: ");
+        int p=ac.nextInt();
+        System.out.println("Enter Rate: ");
+        int r=ac.nextInt();
+        System.out.println("Enter time: ");
+        int t=ac.nextInt();
+        System.out.println("SI : "+((p*r*t)/100));
+
+
+ */
+/*
+//wap to enter product name,quantity,rate and then find total bill
+        System.out.println("Enter Name of the product: ");
+        Scanner ac=new Scanner(System.in);
+        String name=ac.nextLine();
+        System.out.println("Enter Rate: ");
+        int r= ac.nextInt();
+        System.out.println("Enter quantity: ");
+        int q= ac.nextInt();
+        System.out.println("total Bill of "+q+"  "+name + " at rate of "+r+" is : "+(r*q));
+
+
+ */
+        //wap
     }
 }
